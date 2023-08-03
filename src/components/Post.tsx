1777b794615da4a11a8fe6d2290095f2ab441457
Onaopemipo Oluborode <opeoluborode@yahoo.com>
2023-08-03 13:23:37 +0100

    implemented pagination and dynamically rendered the pagination numbers

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -52,27 +52,13 @@ interface ArticlesResponse {
 
 function Posts() {
     const [articles, setArticles] = useState<Article[]>([]);
-    const navigate = useNavigate()
-
-    function formatDate(date: string) {
-        const now = new Date();
-        const createdDate = new Date(Date.parse(date));
-        const difference = createdDate.getTime() - now.getTime();
-
-        const minutes = Math.floor(difference / (1000 * 60));
-        const hours = Math.floor(difference / (1000 * 60 * 60));
-        const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-
-        if (minutes < 60) {
-            return `Posted ${minutes} mins ago`;
-        } else if (hours < 24) {
-            return `Posted ${hours} hours ago`;
-        } else {
-            return `Posted ${days} days ago`;
-        }
-    };
-
-    console.log(formatDate("2023-07-24T13:14:36.000Z"))
+    const [currentPage, setCurrentPage] = useState(1);
+    const [pageStart, setPageStart] = useState(1);
+    const [pageEnd, setPageEnd] = useState(3);
+    const pageSize = 6;
+    const maxPages = 100;
+    const [offset, setOffset] = useState<string | undefined>(undefined);
+    const navigate = useNavigate();
 
     async function fetchArticles(pageSize = 6, offset?: string): Promise<ArticlesResponse> {
         const url = `${import.meta.env.VITE_API_URL}/articles`;
@@ -86,19 +72,39 @@ function Posts() {
         return data as ArticlesResponse;
     }
 
-
     useEffect(() => {
-        fetchArticles().then(data => {
+        fetchArticles(pageSize).then(data => {
             console.log(data.records)
             setArticles(data.records);
         });
     }, []);
 
+    const handleClick = (pageNumber: number) => {
+        setCurrentPage(pageNumber);
+        fetchArticles(pageSize, offset).then(data => {
+            setArticles(data.records);
+            setOffset(data.offset);
+        });
+    }
+
+    const handleNextClick = () => {
+        if (pageEnd < maxPages) {
+            setPageStart(pageStart + 3);
+            setPageEnd(pageEnd + 3);
+        }
+    }
+
+    const handlePrevClick = () => {
+        if (pageStart > 1) {
+            setPageStart(pageStart - 3);
+            setPageEnd(pageEnd - 3);
+        }
+    }
+
     return (
         <section className="blog-news">
             <div className="container">
                 <div className="row gx-5 row-cols-1 row-cols-md-2 se">
-
                     {articles.map(article => (
                         <div className="col mb-4 h-100" key={article.id}>
                             <div className="card border-0">
@@ -114,7 +120,7 @@ function Posts() {
                                         <h3 className="card-title" id="blog-head2">{article.fields.Title}</h3>
                                         <p className="card-text blog-info">{article.fields.Headline}</p>
                                         <div className="d-flex flex-column flex-lg-row justify-content-between">
-                                            <div className="more-info" onClick={ () => navigate(`article/${article.id}`)}><a className="text-decoration-none">
+                                            <div className="more-info" onClick={() => navigate(`article/${article.id}`)}><a className="text-decoration-none">
                                                 <p>Read More<span><img src={Readmore} alt="" /></span></p>
                                             </a></div>
                                             <div className="post-time">
@@ -126,18 +132,16 @@ function Posts() {
                             </div>
                         </div>
                     ))}
-
                 </div>
                 <div className="pagination">
                     <ul className='pag-number'>
-                        <li><img src={PagArrow} className='prev-arrow' alt="" /></li>
-                        <li className='numb active'>1</li>
-                        <li>2</li>
-                        <li>3</li>
+                        <li onClick={handlePrevClick}><img src={PagArrow} className='prev-arrow' alt="" /></li>
+                        {Array.from({ length: pageEnd - pageStart + 1 }, (_, i) => i + pageStart).map(pageNumber => (
+                            <li key={pageNumber} className={pageNumber === currentPage ? 'numb active' : 'numb'} onClick={() => handleClick(pageNumber)}>{pageNumber}</li>
+                        ))}
                         <li>......</li>
-                        <li>20</li>
-                        <li><img src={PagArrow} alt="" /></li>
-
+                        <li onClick={() => handleClick(maxPages)}>{maxPages}</li>
+                        <li onClick={handleNextClick}><img src={PagArrow} className='next-arrow' alt="" /></li>
                     </ul>
                 </div>
             </div>
